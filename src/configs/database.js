1efb2cf72696632dac2d87d7e61ca4f56124a998
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-05 03:29:22 +0600

    bug fixed

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import { envVar } from "./env.variable.js";
 
+// This function connects to the MongoDB database using Mongoose.
 const connectDB = async () => {
   try {
     await mongoose.connect(envVar.dataBaseUrl);
-    console.log("MongoDB connected successfully");
+    console.log("DataBase connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
