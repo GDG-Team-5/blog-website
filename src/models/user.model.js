7fe3dd574d8f91b6e05de5325f61a5644862a4a5
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-05 03:48:24 +0600

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+//user  schema
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+// Middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
