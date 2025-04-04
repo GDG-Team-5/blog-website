dd0a9fdce08a7065c3b0f4eb1e05f760198bd40d
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-05 04:09:05 +0600

    token model implemented

---
+++ src/models/token.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { tokenTypes } from "../configs/token.types.js";
+
+//token schema
+const tokenschema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: Object.values(tokenTypes),
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Token = mongoose.model("Token", tokenschema);
