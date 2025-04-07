ff237a1a6ca353edc964a9732de412089ca86a9c
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-07 15:41:23 +0600

    merged

--- src/utils/custom.error.js
+++ src/utils/custom.error.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CustomError extends Error {
   constructor(statusCode, message, isOperational = false) {
     super(message);
@@ -9,15 +8,3 @@ class CustomError extends Error {
 }
 
 export default CustomError;
-=======
-class CustomError extends Error {
-  constructor(statusCode, message, isOperational = false) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = isOperational;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-
-export default CustomError;
->>>>>>> d7ecc22599000fdc65845647780015dc7cb8f7b8
