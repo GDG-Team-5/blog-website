f4e9a19068cd760b5904d36a6eb5efb18ddc9c8a
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-08 16:47:29 +0600

    bug fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth2";
-import { authService } from "../services/index.js";
 import { handleCatchError } from "../utils/index.js";
 import { User } from "../models/index.js";
 import { authService } from "../services/index.js";
