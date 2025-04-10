f04810846fbfa630659d7abd566c71e0c2be1513
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-10 16:53:54 +0600

    http request,email info, error logger

---
+++ src/configs/request.logger.js
@@ -0,0 +1,29 @@
+import logger from "./wins.logger.js";
+//define morgan format
+const morganFormat =
+  ":remote-user :remote-addr :method :url :status :http-version :response-time ms";
+//define stream option to redirect morgan logs to Logger
+const stream = {
+  write: (data) => {
+    const [
+      userName,
+      ipAddress,
+      method,
+      url,
+      status,
+      httpVersion,
+      responseTime,
+    ] = data.trim().split(" ");
+    return logger.httpLogger.info("HTTP Acess Log", {
+      userName: userName === "-" ? "anonymous" : userName,
+      ipAddress: ipAddress,
+      method: method,
+      url: url,
+      status: Number(status),
+      httpVersion: Number(httpVersion),
+      responseTime: Number(responseTime),
+      timeStamp: new Date().toString(),
+    });
+  },
+};
+export { morganFormat, stream };
---
+++ src/configs/wins.logger.js
@@ -0,0 +1,49 @@
+import { format, transports, createLogger } from "winston";
+const { combine, timestamp, json, simple, colorize } = format;
+import { envVar } from "./env.variable.js";
+
+// file transoport for http logs
+const httpFile = new transports.File({
+  level: "info",
+  filename: `${envVar.fileLogPath}/httpLogs.log`,
+  json: true,
+});
+// file transoport for error logs
+const errFile = new transports.File({
+  filename: `${envVar.fileLogPath}/errorLogs.log`,
+  level: "error",
+});
+//file transport for sent  email logs
+const emailFile = new transports.File({
+  filename: `${envVar.fileLogPath}/emailSent.log`,
+  level: "info",
+});
+// console transport
+const consoleTransport = new transports.Console({
+  format: combine(colorize(), simple()),
+});
+//create http logger
+const httpLogger = createLogger({
+  level: envVar.env === "production" ? "info" : "debug",
+  format: json(),
+  transports: [httpFile, consoleTransport],
+});
+//create error logger
+const errorLogger = createLogger({
+  level: "error",
+  format: combine(json(), timestamp()),
+  transports: [errFile, consoleTransport],
+});
+//create email sent logger
+const emailLogger = createLogger({
+  level: "info",
+  format: combine(json(), timestamp()),
+  transports: [emailFile, consoleTransport],
+});
+// remove console transport if it is in production stage
+if (envVar.env === "production") {
+  httpLogger.remove(consoleTransport);
+  errorLogger.remove(consoleTransport);
+  emailLogger.remove(consoleTransport);
+}
+export default { httpLogger, errorLogger, emailLogger };
