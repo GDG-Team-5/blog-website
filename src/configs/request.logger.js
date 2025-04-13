5da6314045674a75cf47bef2af40599030317cae
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-13 18:34:49 +0600

    refactererd

--- src/configs/request.logger.js
+++ src/configs/request.logger.js
@@ -5,21 +5,11 @@ const morganFormat =
 //define stream option to redirect morgan logs to Logger
 const stream = {
   write: (data) => {
-    const [
-      userName,
-      ipAddress,
-      method,
-      url,
-      status,
-      httpVersion,
-      responseTime,
-    ] = data.trim().split(" ");
+    const [ipAddress, method, url] = data.trim().split(" ");
     return logger.httpLogger.info("HTTP Acess Log", {
-      userName: userName === "-" ? "anonymous" : userName,
       ipAddress: ipAddress,
       method: method,
       url: url,
-      status: Number(status),
     });
   },
 };
